5aae7066d90e8813e33c7f572f4b03f97e5c6f09
Rich Friedel <rich.friedel@gmail.com>
2019-08-14 01:46:59 -0400

    Feature/use androidx (#5)
    * Add new AndroidX packages
    * Update README.md

---
+++ AndroidStudio/FlexboxScratchApp/app/src/main/java/com/ivycreekapps/flexboxscratchapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ivycreekapps.flexboxscratchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.flexbox.FlexboxLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
